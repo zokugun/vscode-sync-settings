9b19fbf4a558f4e75f5f4dd4860232da05e8989c
Baptiste Augrain <daiyam@zokugun.org>
2021-10-21 14:38:49 +0200

    enhance: improve handling of missing settings files and when there is no differences

--- src/commands/view-differences.ts
+++ src/commands/view-differences.ts
@@ -7,6 +7,45 @@ import { Resource } from '../repository';
 import { RepositoryFactory } from '../repository-factory';
 import { Settings } from '../settings';
 
+async function hasDifferences(uriA: Uri, uriB: Uri): Promise<boolean> { // {{{
+	let contentA;
+	let contentB;
+	let deletedA = false;
+	let deletedB = false;
+
+	try {
+		contentA = await fse.readFile(uriA.fsPath, 'utf-8');
+		contentB = await fse.readFile(uriB.fsPath, 'utf-8');
+	}
+	catch {
+		deletedA = true;
+	}
+
+	try {
+		contentB = await fse.readFile(uriB.fsPath, 'utf-8');
+	}
+	catch {
+		deletedB = true;
+	}
+
+	if(deletedA && deletedB) {
+		return false;
+	}
+
+	if(deletedA) {
+		await fse.writeFile(uriA.fsPath, 'This file has been deleted', 'utf-8');
+
+		return true;
+	}
+	else if(deletedB) {
+		await fse.writeFile(uriB.fsPath, 'This file has been deleted', 'utf-8');
+		return true;
+	}
+	else {
+		return contentA !== contentB;
+	}
+} // }}}
+
 export async function viewDifferences(): Promise<void> {
 	const oldRepository = await RepositoryFactory.get();
 
@@ -24,31 +63,43 @@ export async function viewDifferences(): Promise<void> {
 
 		const profileSettings = await oldRepository.loadProfileSettings();
 
+		let identical = true;
+
 		const oldExtensionsUri = Uri.file(oldRepository.getProfileExtensionsPath());
 		const newExtensionsUri = Uri.file(newRepository.getProfileExtensionsPath());
-		if(await fse.readFile(oldExtensionsUri.fsPath, 'utf-8') !== await fse.readFile(newExtensionsUri.fsPath, 'utf-8')) {
+		if(await hasDifferences(oldExtensionsUri, newExtensionsUri)) {
 			await vscode.commands.executeCommand('vscode.diff', oldExtensionsUri, newExtensionsUri, 'extensions.yml', { preview: false });
+
+			identical = false;
 		}
 
 		if(!profileSettings.extends) {
 			if(resources.includes(Resource.Settings)) {
 				const oldUri = Uri.file(oldRepository.getProfileUserSettingsPath());
 				const newUri = Uri.file(newRepository.getProfileUserSettingsPath());
-				if(await fse.readFile(oldUri.fsPath, 'utf-8') !== await fse.readFile(newUri.fsPath, 'utf-8')) {
+				if(await hasDifferences(oldUri, newUri)) {
 					await vscode.commands.executeCommand('vscode.diff', oldUri, newUri, 'settings.json', { preview: false });
+
+					identical = false;
 				}
 			}
 
 			if(resources.includes(Resource.Keybindings)) {
 				const oldUri = Uri.file(oldRepository.getProfileKeybindingsPath(oldRepository.profile, keybindingsPerPlatform));
 				const newUri = Uri.file(newRepository.getProfileKeybindingsPath(newRepository.profile, keybindingsPerPlatform));
-				if(await fse.readFile(oldUri.fsPath, 'utf-8') !== await fse.readFile(newUri.fsPath, 'utf-8')) {
+				if(await hasDifferences(oldUri, newUri)) {
 					await vscode.commands.executeCommand('vscode.diff', oldUri, newUri, 'keybindings.json', { preview: false });
+
+					identical = false;
 				}
 			}
 		}
+
+		if(identical) {
+			await vscode.window.showInformationMessage('There is no differences.', { modal: true });
+		}
 	}
 	else {
-		await vscode.window.showInformationMessage('No differences can be shwon since the repository isn\'t defined.');
+		await vscode.window.showInformationMessage('No differences can be shown since the repository isn\'t defined.', { modal: true });
 	}
 }
