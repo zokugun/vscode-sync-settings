ae328129049fbb86694f9748f5a7034f4598c0fb
Baptiste Augrain <daiyam@zokugun.org>
2022-08-17 15:24:51 +0200

    ci: allow to manually publish

--- scripts/check_versions.sh
+++ scripts/check_versions.sh
@@ -6,5 +6,5 @@ CURRENT_VERSION=$( jq -r .version package.json )
 OPENVSX_VERSION=$( curl -X GET "https://open-vsx.org/api/zokugun/sync-settings" -H "accept: application/json" | jq -r .version )
 
 if [[ "${OPENVSX_VERSION}" != "${CURRENT_VERSION}" ]]; then
-	echo "DEPLOY_OPENVSX=yes" >> "${GITHUB_ENV}"
+	echo "PUBLISH_OPENVSX=yes" >> "${GITHUB_ENV}"
 fi
